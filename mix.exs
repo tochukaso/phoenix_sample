adcef58c7b58323eb03aca1280a5dbc80b2883e8
Omori Yasumitsu <yasumitsuoomori@gmail.com>
2020-02-14 19:09:49 +0900

    [#6] mix.exsの体裁を整える
    Closes #6

--- mix.exs
+++ mix.exs
@@ -4,8 +4,8 @@ defmodule PhoenixSample.MixProject do
   def project do
     [
       app: :phoenix_sample,
-      version: "0.1.0",
-      elixir: "~> 1.5",
+      version: "0.0.1",
+      elixir: "~> 1.9.2",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       start_permanent: Mix.env() == :prod,
@@ -33,16 +33,16 @@ defmodule PhoenixSample.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.4.13"},
-      {:phoenix_pubsub, "~> 1.1"},
-      {:phoenix_ecto, "~> 4.0"},
       {:ecto_sql, "~> 3.1"},
-      {:postgrex, ">= 0.0.0"},
-      {:phoenix_html, "~> 2.11"},
-      {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
+      {:phoenix, "~> 1.4.13"},
+      {:phoenix_ecto, "~> 4.0"},
+      {:phoenix_html, "~> 2.11"},
+      {:phoenix_live_reload, "~> 1.2", only: :dev},
+      {:phoenix_pubsub, "~> 1.1"},
       {:plug_cowboy, "~> 2.0"}
+      {:postgrex, ">= 0.0.0"},
     ]
   end
 
